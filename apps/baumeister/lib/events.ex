490d369f6d37500e922d517b51d49c0c92055806
Klaus Alfert <klaus.alfert@gmail.com>
2016-11-06 12:47:00 +0100

    A minimal event center distributing events to listeners

---
+++ apps/baumeister/lib/events.ex
@@ -0,0 +1,77 @@
+alias Experimental.GenStage
+
+defmodule Baumeister.EventCenter do
+  @moduledoc """
+  This module implements the services required for sending, receiving
+  and dispatching events. It is implemented via the (experimental) `GenStage`
+  module.
+
+  We model out process after `QueueBroadcaster` to allow for
+  various different consumers which might fail and come and go.
+
+  """
+
+  use GenStage
+
+  @type t :: %__MODULE__{
+    queue: :queue.queue,
+    demand: non_neg_integer
+  }
+  defstruct queue: :queue.new, demand: 0
+
+###################################################
+##
+## EventCenter API
+##
+###################################################
+
+ @doc "Starts the EventCenter."
+ @spec start_link() :: {:ok, pid}
+ def start_link() do
+   GenStage.start_link(__MODULE__, :ok, name: __MODULE__)
+ end
+
+ @doc "Sends an event and returns only after the event is dispatched."
+ @spec sync_notify(pid, any, pos_integer | :infinity) :: :ok
+ def sync_notify(pid, event, timeout \\ 5000) do
+   GenStage.call(__MODULE__, {:notify, event}, timeout)
+ end
+
+ @doc "Stops the EventCenter"
+ def stop(pid) do
+   GenStage.stop(pid)
+ end
+ 
+ ###################################################
+ ##
+ ## Callback Implementation
+ ##
+ ###################################################
+
+ def init(:ok) do
+   {:producer, %__MODULE__{}, dispatcher: GenStage.BroadcastDispatcher}
+ end
+
+ def handle_call({:notify, event}, from, state = %__MODULE__{queue: queue}) do
+   added_queue = :queue.in({from, event}, queue)
+   dispatch_events(%__MODULE__{state | queue: added_queue}, [])
+ end
+ def handle_demand(incoming_demand, state = %__MODULE__{demand: pending_demand}) do
+   new_demand = incoming_demand + pending_demand
+   dispatch_events(%__MODULE__{state | demand: new_demand}, [])
+ end
+
+ defp dispatch_events(state = %__MODULE__{queue: queue, demand: 0}, events) do
+   {:noreply, Enum.reverse(events), state}
+ end
+ defp dispatch_events(state = %__MODULE__{queue: queue, demand: demand}, events) do
+   case :queue.out(queue) do
+     {{:value, {from, event}}, new_queue} ->
+       GenStage.reply(from, :ok)
+       dispatch_events(%__MODULE__{queue: new_queue, demand: demand - 1}, [event | events])
+     {:empty, queue} ->
+       {:noreply, Enum.reverse(events), state}
+   end
+ end
+
+end
---
+++ apps/baumeister/test/events_test.exs
@@ -0,0 +1,61 @@
+defmodule EventsTest do
+  use ExUnit.Case
+
+  alias Experimental.GenStage
+  alias Baumeister.EventCenter
+
+  defmodule TestObserver do
+    @moduledoc """
+    A simple observer for the Event Center
+    """
+    use GenStage
+
+    def start(), do: GenStage.start_link(__MODULE__, :ok)
+    def init(_), do: {:consumer, []}
+    def handle_events(events, _, state) do
+      {:noreply, [], state ++ events}
+    end
+
+    def get(stage), do: GenStage.call(stage, :get)
+    def handle_call(:get, _, state), do: {:reply, state, [], state}
+  end
+
+  test "it starts and stops" do
+    {:ok, pid} = EventCenter.start_link()
+    EventCenter.stop(pid)
+  end
+
+  test "observer start and stops" do
+    {:ok, ec} = EventCenter.start_link()
+    {:ok, observer} = TestObserver.start()
+
+    GenStage.sync_subscribe(observer, to: ec)
+    # GenStage.stop(observer)
+    EventCenter.stop(ec)
+    # give the observer some time to die properly
+    Process.sleep(1)
+    refute Process.alive?(observer)
+  end
+
+  test "send and consume an event" do
+    {:ok, ec} = EventCenter.start_link()
+    {:ok, observer} = TestObserver.start()
+    GenStage.sync_subscribe(observer, to: ec)
+
+    :ok = EventCenter.sync_notify(ec, "Hello")
+    assert TestObserver.get(observer) == ["Hello"]
+
+    EventCenter.stop(ec)
+  end
+
+  test "send an event without consumer" do
+    {:ok, ec} = EventCenter.start_link()
+    # {:ok, observer} = TestObserver.start()
+    # GenStage.sync_subscribe(observer, to: ec)
+
+    # expect timeout after 2 ms
+    catch_exit(EventCenter.sync_notify(ec, "Hello", 2))
+
+    EventCenter.stop(ec)
+  end
+end
