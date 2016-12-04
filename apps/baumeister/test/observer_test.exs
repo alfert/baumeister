07c20cf89881e2613e41fde688cc80177dca1227
Klaus Alfert <klaus.alfert@gmail.com>
2016-12-04 09:10:07 +0100

    More tests using a setup function

--- apps/baumeister/test/observer_test.exs
+++ apps/baumeister/test/observer_test.exs
@@ -12,6 +12,7 @@ defmodule Baumeister.ObserverTest do
   alias Baumeister.Observer
   alias Baumeister.Observer.FailPlugin
   alias Baumeister.Observer.NoopPlugin
+  alias Baumeister.Observer.Take
 
   def wait_for(pred) do
     case pred.() do
@@ -22,17 +23,27 @@ defmodule Baumeister.ObserverTest do
     end
   end
 
-  @tag timeout: 1_000
-  test "observe with a failing plugin" do
+  setup context do
+    IO.inspect(context)
     # need process flag, because Observer will crash
     Process.flag(:trap_exit, true)
     {:ok, listener} = TestListener.start()
     GenStage.sync_subscribe(listener, to: Baumeister.EventCenter)
-    {:ok, pid} = Observer.start_link(FailPlugin, :ok)
+    {:ok, pid} = Observer.start_link(context[:test])
     assert is_pid(pid)
 
+    # merge this with the context
+    [pid: pid, listener: listener]
+  end
+
+  @tag timeout: 1_000
+  test "observe with a failing plugin", context do
+    pid = context[:pid]
+    listener = context[:listener]
+
+    Observer.configure(pid, FailPlugin, :ok)
     :ok = Observer.run(pid)
-    # Process.sleep(10)
+
     wait_for fn -> length(TestListener.get(listener)) >= 2 end
 
     l = TestListener.get(listener)
@@ -51,5 +62,44 @@ defmodule Baumeister.ObserverTest do
   # * Introduce `:stop` as addition to `:ok` and `:error`
   # * Think carefully about nesting of plugins to enable
   #   timings, delays, ... Similar to the Plug-Concept.
+
+  @tag timeout: 1_000
+  test "take and noop", context do
+    pid = context[:pid]
+    listener = context[:listener]
+    bmf = """
+    command: echo "Ja, wir schaffen das"
+    """
+
+    Observer.configure(pid, [{Take, 1}, {NoopPlugin, {"file:///", bmf}}])
+    #Observer.configure(pid, [{NoopPlugin, {"file:///", bmf}},{Take, 1}])
+    :ok = Observer.run(pid)
+
+    wait_for fn -> length(TestListener.get(listener)) >= 2 end
+
+    l = TestListener.get(listener)
+    assert length(l) == 2
+    assert [{_, :start_observer, _}, {_, :failed_observer, _}] = l
+  end
+
+
+  @tag timeout: 1_000
+  test "noop", context do
+    pid = context[:pid]
+    listener = context[:listener]
+    bmf = """
+    command: echo "Ja, wir schaffen das"
+    """
+
+    Observer.configure(pid, [{NoopPlugin, {"file:///", bmf}}])
+    :ok = Observer.run(pid)
+
+    wait_for fn -> length(TestListener.get(listener)) >= 2 end
+
+    l = TestListener.get(listener)
+    assert length(l) == 2
+    assert [{_, :start_observer, _}, {_, :failed_observer, _}] = l
+  end
+
 
 end
