5619bba8138a856b21fa5ca49e53df02a7fe9b67
Klaus Alfert <klaus.alfert@gmail.com>
2016-12-27 11:43:20 +0100

    Starting the project management

--- apps/baumeister/lib/baumeister.ex
+++ apps/baumeister/lib/baumeister.ex
@@ -1,7 +1,14 @@
 defmodule Baumeister do
 
+  @moduledoc """
+  This is the connecting module, that combines the observer part of Baumeister
+  with the job execution part. It also provides the main API for Baumeister
+  in terms of embedding it into other applications, such as a Phoenix-based
+  Web UI.
+  """
   alias Baumeister.BaumeisterFile
   alias Baumeister.Observer.Coordinate
+  alias Baumeister.Observer
 
 
   @doc """
@@ -11,6 +18,11 @@ defmodule Baumeister do
   """
   @spec execute(Coordinate.t, BaumeisterFile.t) :: :ok
   def execute(_coord, _job) do
+    :ok
+  end
+
+  @spec add_project(String.t, String.t, [Observer.plugin_config_t]) :: :ok
+  def add_project(project_name, url, plugin_list) do
     :ok
   end
 end
