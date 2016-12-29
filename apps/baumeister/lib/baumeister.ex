b8dcf5136567d18bfcc0889d7dc3243b3d901df8
Klaus Alfert <klaus.alfert@gmail.com>
2016-12-29 17:18:41 +0100

    First attempt of the Baumeister API.

--- apps/baumeister/lib/baumeister.ex
+++ apps/baumeister/lib/baumeister.ex
@@ -9,7 +9,10 @@ defmodule Baumeister do
   alias Baumeister.BaumeisterFile
   alias Baumeister.Observer.Coordinate
   alias Baumeister.Observer
+  alias Baumeister.Config
 
+
+  defstruct name: "", url: "", plugins: [], enabled: false, observer: nil
 
   @doc """
   Executes the commands defined in the BaumeisterFile on a node that
@@ -21,8 +24,36 @@ defmodule Baumeister do
     :ok
   end
 
+  @doc """
+  Adds a new project.
+
+  The `project_name` is the key and must be unique and not used. The `url`
+  must be a valid Git URL, since Git is the only supported repository observer.
+  The `plugin_list` configures the list of observer plugins. They must be given
+  in the wished order of execution.
+  """
   @spec add_project(String.t, String.t, [Observer.plugin_config_t]) :: :ok
-  def add_project(project_name, url, plugin_list) do
-    :ok
+  def add_project(project_name, url, plugin_list) when is_binary(project_name) do
+    case Config.config(project_name) do
+      {:ok, _} -> {:error, "Project #{project_name} already exists"}
+      _ ->
+        {:ok, observer} = Supervisor.start_child(Baumeister.ObserverSupervisor,
+          [project_name])
+        :ok = Observer.configure(observer, plugin_list)
+        :ok = Config.put(project_name,
+          %__MODULE__{name: project_name, url: url, plugins: plugin_list,
+            observer: observer})
+    end
+  end
+
+  @doc """
+  Enables the project and let the observer do its work.
+  """
+  def enable(project_name) do
+    with {:ok, project} = Config.config(project_name),
+      false = project.enabled
+      do
+        :ok = Observer.run(project.observer)
+      end
   end
 end
--- apps/baumeister/test/baumeister_test.exs
+++ apps/baumeister/test/baumeister_test.exs
@@ -3,7 +3,69 @@ defmodule BaumeisterTest do
   doctest Baumeister
   doctest Baumeister.BaumeisterFile
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  alias Baumeister.Config
+  alias Baumeister.Observer.Delay
+  alias Baumeister.Observer.Git
+  alias Baumeister.Observer.FailPlugin
+  alias Baumeister.Observer.NoopPlugin
+  alias Baumeister.Observer.Take
+  alias Baumeister.Test.GitRepos
+  alias Baumeister.EventCenter
+  alias Baumeister.Test.TestListener
+  alias Experimental.GenStage
+
+  require Logger
+  @moduletag capture_log: true
+
+  # Setup the repository and the paths to their working spaces
+  setup do
+    Logger.info "Stop the Observer Supervisor"
+    :ok = Supervisor.stop(Baumeister.ObserverSupervisor, :normal)
+    repos = GitRepos.make_temp_git_repo_with_some_content()
+    assert nil != Process.whereis(Baumeister.ObserverSupervisor)
+    {:ok, repos}
+  end
+
+  test "add a new project", %{parent_repo_path: repo_url, parent_repo: repo} do
+    # observe the git repo, but only 1 time, and wait 100 ms
+    plugs = [{Delay, 100}, {Git, repo_url}, {Take, 1}]
+    project = "baumeister_test"
+    assert Config.keys() == []
+
+    {:ok, observer} = TestListener.start()
+    GenStage.sync_subscribe(observer, to: EventCenter)
+
+    :ok = Baumeister.add_project(project, repo_url, plugs)
+
+    # nothing has happend, the observer is still disabled
+    assert [] == TestListener.get(observer)
+    assert Config.keys() == [project]
+
+    # enable the observer
+    :ok = Baumeister.enable(project)
+
+    {bmf, _local_os} = create_bmf("echo Hello")
+    {:ok, _} = GitRepos.update_the_bmf(repo, bmf)
+
+    assert [] == TestListener.get(observer)
+
+  end
+
+  test "Ensure that all required processes are running" do
+    obs_sup = Process.whereis(Baumeister.ObserverSupervisor)
+    assert nil != obs_sup
+    assert 0 == Supervisor.count_children(obs_sup).active
+    assert nil != Process.whereis(Baumeister.ObserverTaskSupervisor)
+  end
+
+  def create_bmf(cmd \\ "true") do
+    {_, local_os} = :os.type()
+    local_os = local_os |> Atom.to_string
+    bmf = """
+      os: #{local_os}
+      language: elixir
+      command: #{cmd}
+    """
+    {bmf, local_os}
   end
 end
