a977771b5b12f0a37ce7332b117779854660ac14
Klaus Alfert <klaus.alfert@gmail.com>
2017-01-22 17:10:32 +0100

    Added distillery for making releases

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BaumeisterUmbrella.Mixfile do
     [apps_path: "apps",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps()]
   end
 
   # Dependencies can be Hex packages:
@@ -20,7 +20,7 @@ defmodule BaumeisterUmbrella.Mixfile do
   #
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps folder
-  defp deps do
-    []
+  defp deps() do
+    [{:distillery, "~> 1.0"}]
   end
 end
