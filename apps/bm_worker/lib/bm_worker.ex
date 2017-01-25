521ceba1c55a097918c106ef1a166637ebd5e1f7
Klaus Alfert <klaus.alfert@gmail.com>
2017-01-25 07:00:04 +0100

    new sub application for a worker node to ease releases

---
+++ apps/bm_worker/lib/bm_worker.ex
@@ -0,0 +1,18 @@
+defmodule BmWorker do
+  @moduledoc """
+  Documentation for BmWorker.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BmWorker.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
