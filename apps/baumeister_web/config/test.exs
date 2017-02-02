1c485ac270a6e817c01fc36ce532e2c7b00226bb
Klaus Alfert <klaus.alfert@gmail.com>
2017-02-02 23:31:12 +0100

    Tests run in memory only

--- apps/baumeister_web/config/test.exs
+++ apps/baumeister_web/config/test.exs
@@ -12,6 +12,10 @@ config :logger, level: :warn
 # Configure your database
 config :baumeister_web, BaumeisterWeb.Repo,
   adapter: Ecto.Adapters.Mnesia
+config :ecto_mnesia,
+    host: {:system, :atom, "MNESIA_HOST", Kernel.node()},
+    storage_type: {:system, :atom, "MNESIA_STORAGE_TYPE", :ram_copies}
+
   # adapter: Ecto.Adapters.Postgres,
   # username: "postgres",
   # password: "postgres",
