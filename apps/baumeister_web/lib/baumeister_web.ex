bd9e2336842009d515a33227f2c26340a1faa39f
Klaus Alfert <klaus.alfert@gmail.com>
2017-02-15 23:42:02 +0100

    Use startup phases for starting and setting up mnesia first.

--- apps/baumeister_web/lib/baumeister_web.ex
+++ apps/baumeister_web/lib/baumeister_web.ex
@@ -1,10 +1,14 @@
 defmodule BaumeisterWeb do
   use Application
 
+  require Logger
+
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
-  def start(_type, _args) do
+  def start(type, args) do
     import Supervisor.Spec
+
+    Logger.info("BaumeisterWeb.start(type=#{inspect type}, args=#{inspect args})")
 
     # Define workers and child supervisors to be supervised
     children = [
@@ -26,6 +30,21 @@ defmodule BaumeisterWeb do
   # whenever the application is updated.
   def config_change(changed, _new, removed) do
     BaumeisterWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+
+  @doc """
+  Different Start Phases for configuring and starting
+  """
+  def start_phase(:mnesia_up, _, _) do
+    Logger.info("BaumeisterWeb.start_phase(:mnesia_up, _, _)")
+    BaumeisterWeb.Release.Tasks.create_mnesia_db()
+    BaumeisterWeb.Release.Tasks.migrate()
+    :ok
+  end
+  def start_phase(start_phase, start_type, args) do
+    Logger.info("BaumeisterWeb.start_phase(start_phase=#{inspect start_phase}, "
+        <> "start_type=#{inspect start_type}, args=#{inspect args})")
     :ok
   end
 end
