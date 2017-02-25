88aebce88057ae839048f91efe9741029c2993b9
Klaus Alfert <klaus.alfert@gmail.com>
2017-02-25 19:36:34 +0100

    Only allow show and index routes for Builds

--- apps/baumeister_web/web/router.ex
+++ apps/baumeister_web/web/router.ex
@@ -18,7 +18,7 @@ defmodule BaumeisterWeb.Router do
 
     get "/", PageController, :index
     resources "/projects", ProjectController
-    resources "/builds", BuildController
+    resources "/builds", BuildController, only: [:index, :show]
   end
 
   # Other scopes may use custom stacks.
