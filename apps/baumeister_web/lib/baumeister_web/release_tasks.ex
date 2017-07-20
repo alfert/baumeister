355c1ce011ca71c2e684a926ea38ee2bdee501b3
Klaus Alfert <klaus.alfert@gmail.com>
2017-07-20 20:58:08 +0200

    Rename the Mnesia Ecto Adapter calls properly (Breaking change in 0.8 of ecto_mnesia)

--- apps/baumeister_web/lib/baumeister_web/release_tasks.ex
+++ apps/baumeister_web/lib/baumeister_web/release_tasks.ex
@@ -10,7 +10,7 @@ defmodule BaumeisterWeb.Release.Tasks do
   """
   def create_mnesia_db do
     Logger.info "Stopping Mnesia for creating the schema"
-    Ecto.Mnesia.Storage.stop()
+    EctoMnesia.Storage.stop()
 
     # ensure the database directory
     dir = "#{Confex.get(:mnesia, :dir)}"
@@ -21,19 +21,19 @@ defmodule BaumeisterWeb.Release.Tasks do
     if :mnesia.system_info(:use_dir) do
       Logger.info "Mnesia dir is in use already"
     else
-      conf = Ecto.Mnesia.Storage.conf()
+      conf = EctoMnesia.Storage.conf()
       Logger.info("Start Mnesia for Schema creation with conf: #{inspect conf}")
-      # Ecto.Mnesia.Storage.start()
+      # EctoMnesia.Storage.start()
 
       Logger.info "Create Schema with storage up"
-      case Ecto.Mnesia.Storage.storage_up(conf) do
+      case EctoMnesia.Storage.storage_up(conf) do
         :ok -> Logger.info "Schema created"
         {:ok, :already_created} -> Logger.info "Schema was already created"
         {:error, :already_up} -> Logger.error "Mnesia was already up -> continue!"
       end
       # stop mnesia for restarting later when the application starts
       Logger.info "Stopping Mnesia after schema creation"
-      Ecto.Mnesia.Storage.stop()
+      EctoMnesia.Storage.stop()
     end
   end
 
@@ -44,7 +44,7 @@ defmodule BaumeisterWeb.Release.Tasks do
     #Logger.info "Ensure that :baumeister_web is started"
     # {:ok, _} = Application.ensure_all_started(:baumeister_web)
     Logger.info("Start Mnesia for migration")
-    :ok = Ecto.Mnesia.Storage.start()
+    :ok = EctoMnesia.Storage.start()
     Logger.info "Waiting for all tables to come back"
     :ok = :mnesia.wait_for_tables(:mnesia.system_info(:tables), 5_000)
 
