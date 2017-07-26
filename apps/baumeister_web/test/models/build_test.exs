60699cc735ef8dcb99be693731c1505b9d51b613
Klaus Alfert <klaus.alfert@gmail.com>
2017-07-26 15:51:11 +0200

    Ensure that the build ID is autogenerated and numeric.

--- apps/baumeister_web/test/models/build_test.exs
+++ apps/baumeister_web/test/models/build_test.exs
@@ -6,6 +6,18 @@ defmodule BaumeisterWeb.Web.BuildTest do
   @valid_attrs %{config: "some content", coordinate: "some content", log: "some content", number: 42, project_id: 42}
   @invalid_attrs %{}
 
+  setup do
+    {_, _} = Repo.delete_all(Build)
+    :ok
+  end
+
+  test "store build with valid attributes" do
+    changeset = Build.changeset(%Build{}, @valid_attrs)
+    assert changeset.valid?
+    {:ok, build} = Repo.insert(changeset)
+    assert build.id == 1
+  end
+
   test "changeset with valid attributes" do
     changeset = Build.changeset(%Build{}, @valid_attrs)
     assert changeset.valid?
@@ -14,5 +26,9 @@ defmodule BaumeisterWeb.Web.BuildTest do
   test "changeset with invalid attributes" do
     changeset = Build.changeset(%Build{}, @invalid_attrs)
     refute changeset.valid?
+  end
+
+  test "there are no builds to load" do
+    assert [] = Repo.all(Build)
   end
 end
